7146b61a278b016f86d2dc7bfd044c5843482177
Sean <linyongping@outlook.com>
2017-12-13 23:43:26 +0800

    优化css

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,21 +1,41 @@
-import React from 'react';
-import { connect } from 'dva';
-import styles from './IndexPage.css';
+import React from "react";
+import { connect } from "dva";
+import styles from "./IndexPage.css";
+
+const cheetSheets = [
+  { title: "Javascript", link: "http://overapi.com/javascript#more" },
+  { title: "Vim", link: "https://vim.rtorr.com/" },
+  { title: "ES2015", link: "https://devhints.io/es6" },
+  {
+    title: "JSRegex",
+    link: "https://www.debuggex.com/cheatsheet/regex/javascript"
+  },
+  { title: "Flexbox", link: "https://yoksel.github.io/flex-cheatsheet/" },
+  { title: "Less", link: "https://devhints.io/less" },
+  {
+    title: "MarkDown",
+    link: "https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet"
+  }
+];
+
+const Link = props => (
+  <li>
+    <div className="awrapper">
+      <a href={props.link}>
+        <span className="thin"> {props.title} </span>
+      </a>
+    </div>
+  </li>
+);
 
 function IndexPage() {
   return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to dva!</h1>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-        <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
-      </ul>
+    <div>
+      <ul>{cheetSheets.map(item => <Link key={item.link} {...item} />)}</ul>
     </div>
   );
 }
 
-IndexPage.propTypes = {
-};
+IndexPage.propTypes = {};
 
 export default connect()(IndexPage);
